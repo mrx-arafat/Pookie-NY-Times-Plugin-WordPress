b6849c86aa74bb80420a4fbb6f5fb30f80b7aa4f
mrx-arafat <arafatmrx@gmail.com>
2024-08-17 11:55:55 +0600

    add_option_page

--- pgnyt_articles.php
+++ pgnyt_articles.php
@@ -10,27 +10,24 @@ Author URI: http://wvm.patrickgerrits.com
 License: GPL2
 */
 
-// function pgnyt_articles_menu()
-// {
-//     add_options_page(
-//         'NY Times Articles Options',   // Page title
-//         'NY Times Articles',           // Menu title
-//         'manage_options',              // Capability required
-//         'pgnyt-articles',              // Menu slug
-//         'pgnyt_articles_options'       // Callback function to display the page content
-//     );
-// }
+function pgnyt_articles_menu()
+{
+    add_options_page(
+        'NY Times Articles Options',   // Page title
+        'NY Times Articles',           // Menu title
+        'manage_options',              // Capability required
+        'pgnyt-articles',              // Menu slug
+        'pgnyt_articles_options'       // Callback function to display the page content
+    );
+}
 
-// add_action('admin_menu', 'pgnyt_articles_menu');
+add_action('admin_menu', 'pgnyt_articles_menu');
 
-// function pgnyt_articles_options()
-// {
-//     // This function will display the options page content.
-//     echo '<div class="wrap">';
-//     echo '<h1>NY Times Articles Settings</h1>';
-//     echo '<p>Here you can set all the options for the NY Times articles plugin.</p>';
-//     echo '</div>';
-// }
-
-
-?>;
+function pgnyt_articles_options()
+{
+    // This function will display the options page content.
+    echo '<div class="wrap">';
+    echo '<h1>NY Times Articles Settings</h1>';
+    echo '<p>Here you can set all the options for the NY Times articles plugin.</p>';
+    echo '</div>';
+}
